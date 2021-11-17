9cecd174b9b5672d7e0f54c852e4fbc8f1d13e0b
andna5980 <andy5980@hotmail.com>
2021-11-17 11:54:39 +0000

    install coverage to check all tests

--- todo/test_models.py
+++ todo/test_models.py
@@ -8,3 +8,8 @@ class TestModels(TestCase):
         item = Item.objects.create(name='Test Todo Item')
         self.assertFalse(item.done)
 
+    def test_item_string_method_retunrs_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
+
+
